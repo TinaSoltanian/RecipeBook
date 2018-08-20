da926235f6ddb25cb58ce17bbead68f36e5a17b7
Tina <tinasoltanian@gmail.com>
2018-08-20 00:39:56 -0700

    create components

--- recipebook/src/app/app.module.ts
+++ recipebook/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { HeaderComponent } from './header/header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    ShoppingEditComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
