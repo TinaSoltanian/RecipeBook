b070f3133fe6c814eae1f0f54d2067d8665b15b8
Tina <tinasoltanian@gmail.com>
2018-09-14 00:30:05 -0700

    * move recipe item into it's component from recipe list * pass teh recipe item value to the recipe item to decorate in the recipe item component

--- recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -17,6 +17,11 @@ export class RecipeListComponent implements OnInit {
       'Test recipe',
       'This is description',
       'https://media.self.com/photos/57dff8aa7160f6ee33314fdf/8:3/w_1280,c_limit/sub-channel-food_recipes.jpg'
+    ),
+    new Recipe(
+      'Test recipe 2',
+      'This is description',
+      'https://media.self.com/photos/57dff8aa7160f6ee33314fdf/8:3/w_1280,c_limit/sub-channel-food_recipes.jpg'
     )
   ];
   constructor() {}
