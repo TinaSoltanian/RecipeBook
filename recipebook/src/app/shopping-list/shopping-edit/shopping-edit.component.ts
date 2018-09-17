bb66fcd0b92076df4127e70de8febce842937c2a
Tina <tinasoltanian@gmail.com>
2018-09-16 19:18:10 -0700

    Add user input to shopping list, I used ViewChild and local reference to access the DOM elements for now

--- recipebook/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipebook/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('amountInput') amountInput: ElementRef;
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @Output() onAdd = new EventEmitter<Ingredient>();  
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddClicked() {
+    if (this.amountInput.nativeElement.value !== '' && this.nameInput.nativeElement.value !== ''){
+
+        this.onAdd.emit( new Ingredient( this.nameInput.nativeElement.value, 
+        this.amountInput.nativeElement.value ) );
+
+        this.nameInput.nativeElement.value = '';
+        this.amountInput.nativeElement.value = '';
+    }    
+  }
 }
