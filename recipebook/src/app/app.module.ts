5dc51f70812b8af23d5f83e4ae6f2003e095b6a4
Tina <tinasoltanian@gmail.com>
2019-02-18 15:55:15 -0800

    Providing the recipe service in app.module so we alway gonne use the same unique service all the time and it is not destroing and recreating itself

--- recipebook/src/app/app.module.ts
+++ recipebook/src/app/app.module.ts
@@ -9,13 +9,12 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { DropdownDirective } from './shared/dropdown.directive';
-import { RecipeService } from './recipes/recipe.service';
 import { ShoppingService } from './shopping-list/shopping.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
   declarations: [
--- recipebook/src/app/recipes/recipes.component.ts
+++ recipebook/src/app/recipes/recipes.component.ts
@@ -1,17 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
-import { RecipeService } from './recipe.service';
 
 @Component({
-  providers: [RecipeService],
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() { }
 
