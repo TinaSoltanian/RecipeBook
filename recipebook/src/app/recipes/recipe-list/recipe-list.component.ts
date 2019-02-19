3598ab618e9ecdc4b597e149f40543c96b18e993
Tina <tinasoltanian@gmail.com>
2019-02-18 16:04:21 -0800

    implement onDestroy for recipe list to prevent memory licking

--- recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {
-    this.recipeService.recipeChanged.subscribe(
+    this.subscription = this.recipeService.recipeChanged.subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
       }
     );
 
     this.recipes = this.recipeService.getAllRecipes();
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 }
